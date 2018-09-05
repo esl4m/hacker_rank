48f62cbb262ae77690306e68bce8b3e4186b15e5
Eslam Elgamal <me@esl4m.com>
2018-09-05 12:30:05 +0300

    Edits in code

--- mix/linear_search.py
+++ mix/linear_search.py
@@ -20,13 +20,10 @@ print(search(arr, x=175))
 
 
 # Examples :
-# Input : arr[] = {10, 20, 80, 30, 60, 50,
-#                      110, 100, 130, 170}
-#           x = 110;
-# Output : 6
-# Element x is present at index 6
+# Input : arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+# x = 110
+# Output : 6 ... Element x is present at index 6
 #
-# Input : arr[] = {10, 20, 80, 30, 60, 50,
-#                      110, 100, 130, 170}
-#            x = 175;
+# Input : arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+# x = 175
 # Output : -1 ... Element x is not present in arr[].
