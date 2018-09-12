b817ee788752618b7e62bd7edbd9a4eb9eefe65f
Eslam Elgamal <me@esl4m.com>
2018-09-12 13:41:22 +0300

    Adds string recursion algorithm - python

---
+++ mix/string_recursive.py
@@ -0,0 +1,15 @@
+# Write a function that takes some words and return the recursive string
+
+
+def string_recursion(s):
+    str_arr = s.split()  # convert the string input to arr.
+    res = []
+    for w in str_arr[::-1]:
+        res.append(w)
+
+    res = " ".join(res)  # convert the result to string
+
+    return res
+
+main_str = "Dogs bites men"
+print(string_recursion(main_str))
