0cdf5fee6f18c89c57d80f63af6e54f3dbfd47c9
Eslam Elgamal <me@esl4m.com>
2020-03-26 14:23:51 +0300

    Enhancements in sum all - add global

--- sum_all_numbers.py
+++ sum_all_numbers.py
@@ -5,20 +5,35 @@
 #      Mar11 2020
 
 
-class MySum:
-    def __init__(self):
-        #
-        self.start = 0
+# class MySum:
+#     def __init__(self):
+#         #
+#         self.start = 0
+#
+#     def sum_them(self):
+#         res = 0
+#         numbers = [1, 2, 3, 4, 5, 8]
+#
+#         for x in numbers:
+#             res = self.start + x  # result will be self + the current number (x) .. at first self.start is zero ;)
+#             self.start = res  # changing the start value to be the current result.
+#
+#         return res
+#
+# s = MySum()
+# print(s.sum_them())
 
-    def sum_them(self):
-        res = 0
-        numbers = [1, 2, 3, 4, 5, 8]
+start = 0
 
-        for x in numbers:
-            res = self.start + x  # result will be self + the current number (x) .. at first self.start is zero ;)
-            self.start = res  # changing the start value to be the current result.
+def sum_them():
+    global start
+    res = 0
+    numbers = [1, 2, 3, 4, 5, 8]
 
-        return res
+    for x in numbers:
+        res = start + x  # result will be self + the current number (x) .. at first self.start is zero ;)
+        start = res  # changing the start value to be the current result.
 
-s = MySum()
-print(s.sum_them())
+    return res
+
+print(sum_them())
