5bc7c33dce81f1287add74f6b366697ee8bdd49f
Eslam Elgamal <me@esl4m.com>
2020-10-04 18:16:29 +0300

    Adds is_sorted search algorithm

---
+++ InterviewPreparationKit/search/is_sorted.py
@@ -0,0 +1,16 @@
+# check if array is sorted or not before search
+
+
+items1 = [6, 8, 19, 20, 23, 41, 49, 53, 56, 87]
+items2 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+
+def is_sorted(itemlist):
+    # for i in range(0, len(itemlist)-1):
+    #     if (itemlist[i] > itemlist[i+1]):
+    #         return False
+    # return True
+
+    return all(itemlist[i] <= itemlist[i+1] for i in range(len(itemlist)-1))
+
+print(is_sorted(items1))
+print(is_sorted(items2))
