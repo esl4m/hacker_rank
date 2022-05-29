1d6b315c3a5843ba4d1e5788f3a1b7ccfdc9ee19
esl4m <me@esl4m.com>
2022-05-29 23:50:12 +0200

    Adds sorting - bubble sort challenges

---
+++ InterviewPreparationKit/sort/bubble_sort.py
@@ -0,0 +1,21 @@
+
+def bubble_sort(dataset):
+    # TODO: start with the array
+    for i in range (len(dataset) -1, 0, -1):
+        for j in range(i):
+            if dataset[j] > dataset[j+1]:
+                temp = dataset[j]
+                dataset[j] = dataset[j+1]
+                dataset[j+1] = temp
+
+        print("Current state: ", dataset)
+
+
+def main():
+    list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+    bubble_sort(list1)
+    print("Result: ", list1)
+
+
+if __name__ == "__main__":
+    main()
