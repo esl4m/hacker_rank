703942b1e4ae7cadb3eab5193e032c8b26b2fbaf
Eslam Elgamal <me@esl4m.com>
2022-06-03 23:34:01 +0200

    Add sorting challenge comparator

---
+++ InterviewPreparationKit/sort/comparator.py
@@ -0,0 +1,48 @@
+from functools import cmp_to_key
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __repr__(self):
+        pass
+
+    def comparator(a, b):
+        if a.score < b.score:
+            return 1
+        elif a.score > b.score:
+            return -1
+        else:  # score are equal
+            if a.name > b.name:
+                return 1
+            elif a.name < b.name:
+                return -1
+            else:
+                return 0
+
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+    
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
+
+# Sample input
+# 5
+# amy 100
+# david 100
+# heraldo 50
+# aakansha 75
+# aleksa 150
+
+# Sample Output
+# aleksa 150
+# amy 100
+# david 100
+# aakansha 75
+# heraldo 50
