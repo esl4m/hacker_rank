e1e597b2360e97d283b7070fd9576a75048a0c11
esl4m <me@esl4m.com>
2022-06-27 17:32:39 +0200

    Adds - Practice python pyramid

---
+++ mix/python_pyramid.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+# python patterns
+def py_pyramid(n):
+    for i in range(0, n):
+        for j in range (0, i+1):
+            print('* ', end= ' ')
+        print('\r')
+
+n = 5
+py_pyramid(n)
+
+
+# Output
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
