1bb77ed92370e42e520d8dd75432c01e71408e9f
esl4m <me@esl4m.com>
2022-06-28 01:14:08 +0200

    Updates python pyramid - adds another way (list)

--- mix/python_pyramid.py
+++ mix/python_pyramid.py
@@ -1,14 +1,26 @@
 #!/bin/python3
 
-# python patterns
+# python pyramid
 def py_pyramid(n):
     for i in range(0, n):
         for j in range (0, i+1):
-            print('* ', end= ' ')
+            print('*', end= ' ')
         print('\r')
 
 n = 5
 py_pyramid(n)
+
+
+# python pyramid list
+def py_pyramid_list(n):
+    my_list = []
+    for i in range(0, n+1):
+        my_list.append("* " * i)
+
+    print("\n".join(my_list))
+
+n = 5
+py_pyramid_list(n)
 
 
 # Output
